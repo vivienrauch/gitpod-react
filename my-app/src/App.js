2c413755de55bf0fda95f5c72abc0899b9df65e4
vivienrauch <vivienmirjam@gmail.com>
2023-06-19 08:29:22 +0000

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+
+function App() {
+  return (
+    <div className="App">
+        <FunctionalGreetingWithProps />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+//function FunctionalGreetingWithProps() {
+//    return <h1>Hello from React!</h1>
+//}
+
+const FunctionalGreetingWithProps = () => <h1>Hello!</h1>;
+
+export default FunctionalGreetingWithProps;
