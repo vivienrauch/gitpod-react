78bc5e9fe7339e6dd1c9d95dc462c317a1cf1e8d
vivienrauch <vivienmirjam@gmail.com>
2023-06-20 08:49:14 +0000

    Add class handling

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -8,14 +8,38 @@ class StatefulGreeting extends React.Component {
             //initial state
             introduction: "Hello!",
             buttonText: "Exit",
+            count: 0
         };
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+            console.log('previous State:', prevState)
+            console.log('previous Props:', prevProps)
+            return {
+                introduction: prevState.introduction === "Hello" ? "Goodbye" : "Hello",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit"
+            }
+        })
+    }
+
+    increment() {
+        this.setState((prevState, prevProps) => {
+            console.log('previous State:', prevState)
+            console.log('previous Props:', prevProps)
+            return {
+                count: prevState.count + 1
+            }
+        })
     }
 
     render() {
         return (
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
-                <button>{this.state.buttonText}</button>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.increment()}>Increment</button>
+                <p>You've clicked {this.state.count} times</p>
             </div>
         )
     }
