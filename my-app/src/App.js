676d26d935c2c68aeced8cc3f621cf2f136a89a1
vivienrauch <vivienmirjam@gmail.com>
2023-06-26 08:01:53 +0000

    test conditional rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import EventsClass from "./components/EventsClass";
-import EventsFunctional from "./components/EventsFunctional";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 
 function App() {
-  return <div className="App">
-    <EventsFunctional />
-    <EventsClass />
-  </div>
+  return (<div className="App">
+    <ConditionalRenderingClass />
+    <ConditionalRenderingFunctional connected={false} />
+  </div>); 
 }
 
 export default App;
